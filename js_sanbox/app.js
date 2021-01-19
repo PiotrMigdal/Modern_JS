e7f4bad8f9bbff5788c91389d70dc3879cd256ad
PiotrMigdal <PiotrMigdal@users.noreply.github.com>
2021-01-19 08:04:29 +0000

    Lesson 26

--- js_sanbox/app.js
+++ js_sanbox/app.js
@@ -1,3 +1,6 @@
+//LESSON 1
+//Just go through the document. selector and what it can do
+/*
 let val;
 
 val = document;
@@ -36,5 +39,140 @@ let scriptsArr = Array.from(scripts);
 scriptsArr.forEach(function(script) {
   console.log(script.getAttribute('src'));
 });
+
+console.log(val);
+*/
+
+//LESSON 2
+
+//document.getElementById();
+/*
+console.log(document.getElementById('task-title'));
+//get its things
+console.log(document.getElementById('task-title').id);
+console.log(document.getElementById('task-title').className);
+//change style
+document.getElementById('task-title').style.background = '#000';
+document.getElementById('task-title').style.color = '#fff';
+document.getElementById('task-title').style.padding = '20px';
+//change content
+document.getElementById('task-title 5').textContent = 'Task list';
+//we should use variable for such iterations
+const taskTitle = document.getElementById('task-title');
+taskTitle.innerText = 'List';
+taskTitle.innerHTML = '<p>TASKS</p>';
+
+//document.querySelector()
+//you can select anything class, id, tag but only one (first) on page
+
+console.log(document.querySelector('h5'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('#task-title'));
+//you can select nth-child
+document.querySelector('li:nth-child(3)').style.color = 'red';
+document.querySelector('li:nth-child(3)').innerText = 'Czerwony';
+
+*/
+//Lesson 25 DOM selectors for multiple elements
+/*
+const items = document.getElementsByClassName('collection-item');
+console.log(items);
+//it is array of items you can select one
+items[0].style.color = 'Blue'; //slected first
+//you can also specify the certain list
+const itemsList = document.querySelector('ul').getElementsByClassName('collection-item');
+itemsList[2].style.color = 'yellow';
+
+//document.getElementsByTagName
+let lis = document.getElementsByTagName('li');
+console.log(lis);
+lis[1].style.color = 'purple';
+
+console.log(typeof lis); //it is object HTMLCollection
+
+//you can use for loop even on htmlcollection
+
+for(let i = 0; i < lis.length; i++){
+  lis[i].textContent = 'for';
+}
+
+//to do forEach you need to convert to array
+lis = Array.from(lis);
+lis.reverse();
+
+//add index
+lis.forEach(function(li, index){
+  li.textContent = (`${index}. HELLO`);
+});
+
+
+//document.querySelectorAll();
+const items = document.querySelectorAll('ul li.collection-item');
+
+items.forEach(function(item){
+  item.style.color = 'orange';
+});
+
+//change odds using forEach loop
+const itemsOdd = document.querySelectorAll('ul li.collection-item:nth-child(odd)');
+
+itemsOdd.forEach(function(item){
+  item.style.background = '#ccc';
+});
+
+//change evens using for loop
+const itemsEven = document.querySelectorAll('ul li.collection-item:nth-child(even)');
+
+for(let i=0; i < itemsEven.length; i++){
+  itemsEven[i].style.background = '#e3e3e3';
+}
+
+*/
+
+//LESSON 26 traversing the DOM
+
+let val;
+const list = document.querySelector('ul.collection');
+
+//just example how childNodes works - you will not use it often
+val = list.childNodes; //it returns all together with spaces between Li
+val = list.childNodes[0]; //gives only text (this is enter between Lis)
+val = list.childNodes[0].nodeName;
+val = list.childNodes[0].nodeType; //gives node type index - returned 3
+//1 - element
+//2 - attribute (deprecated)
+//3 - Text node
+//8 - comment
+//9 - document itself
+//10 - doctype
+
+
+//CHILDREN ELEMENT NODES - more useful
+val = list.children;
+val = list.children[0];
+list.children[0].textContent = 'HTML';
+//children of children
+val = list.children[3].children[0]; //you can take children of children
+list.children[3].children[0].id = 'new-id';
+//first child
+val = list.lastElementChild;
+//last child
+val = list.firstElementChild;
+//count child
+val = list.childElementCount;
+
+
+//GET PARENTS
+const listItem = document.querySelector('li.collection-item:first-child');
+val = listItem.parentElement;
+//parent of parent
+val = listItem.parentElement.parentElement;
+
+//GET SIBLINGS
+//next sibling
+val = listItem.nextElementSibling;
+//prev sibling
+val = listItem.previousElementSibling;
+
 
 console.log(val);
